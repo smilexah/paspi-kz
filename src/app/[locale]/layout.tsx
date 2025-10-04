56ebac1f13fa7513995e5b25ea2ca1ca499f8546
Meiirzhan Muratbekuly <smilexah@inbox.ru>
2025-10-04 07:26:17 +0500

    Update metadata, add favicon, and create web manifest for Paspi.kz

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -10,8 +10,8 @@ import {Footer} from "@/components/shared/footer";
 
 export const metadata: Metadata = {
     title: "Kaspi.kz",
-    description: "Kaspi Bank - ведущий банк Казахстана, предлагающий инновационные финансовые решения для частных лиц и бизнеса.",
-    keywords: ["Kaspi Bank", "Каспи Банк", "банк", "финансы", "Казахстан", "инновации", "услуги", "кредитование", "инвестиции", "онлайн-банкинг"],
+    description: "Покупка в кредит, Оплата услуг, Kaspi Bank, Переводы и конвертация, Выписка по картам, Остаток на депозите, Задолженность по кредиту, Покупки в рассрочку, Kaspi Бонус, Вопросы и ответы",
+    keywords: ["Kaspi Bank", "Каспи Банк", "Kaspi Магазин", "рассрочка", "покупки", "Платежи", "Kaspi Кошелек", "Kaspi Бонус", "Кредит Наличными", "Товарный кредит", "Kaspi Депозит", "Кредитная Карта", "Kaspi Gold", "Kaspi Red", "Kaspi Гид", "вопросы", "ответы"],
     authors: [{name: "Kaspi Bank", url: "https://kaspi.kz"}],
 };
 
@@ -32,6 +32,15 @@ export default async function LocaleLayout({
 
     return (
         <html lang={locale}>
+        <head>
+            <title>Kaspi.kz – Магазин, Платежи, Мой Банк, Переводы, Red, Бонусы, Гид</title>
+            <link rel="icon" type="image/png" href="/favicons/favicon-96x96.png" sizes="96x96" />
+            <link rel="icon" type="image/svg+xml" href="/favicons/favicon.svg" />
+            <link rel="shortcut icon" href="/favicons/favicon.ico" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+            <meta name="apple-mobile-web-app-title" content="Paspi.kz" />
+            <link rel="manifest" href="/favicons/site.webmanifest" />
+        </head>
         <body className={`antialiased`}>
         <NextIntlClientProvider messages={messages}>
             <Header/>
